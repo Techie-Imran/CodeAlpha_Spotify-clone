314b406d78802e7669add8db6bc1469e1151d558
Techie-Imran <tajimran295@gmail.com>
2025-07-25 00:51:30 +0530

    Initial commit - Spotify Clone

---
+++ src/assets/assets.js
@@ -0,0 +1,208 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
+import img11 from './img11.jpg'
+import img12 from './img12.jpg'
+import img13 from './img13.jpg'
+import img14 from './img14.jpg'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+
+import Bff1 from './Bff1.jpg'
+import Heat1 from './Heat1.jpg'
+import iwbu1 from './iwbu1.jpg'
+import Shu1 from './Shu1.jpg'
+import Starimg from './Starimg.jpg'
+import Txc1img from './Txc1img.jpg'
+import Sail1 from './Sail1.jpg'
+import Sun1 from './Sun1.jpg'
+
+import Bff from  './Bff.mp3'
+import Heat from  './Heat.mp3'
+import Iwbu from  './Iwbu.mp3'
+import Shu from  './Shu.mp3'
+import Star from  './Star.mp3'
+import Txc from  './Txc.mp3'
+import Sail from  './Sail.mp3'
+import Sun from  './Sun.mp3'
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Global",
+        image: img8,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "Top 50 India",
+        image: img9,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#22543d"
+    },
+    {   
+        id:2,
+        name: "Trending India",
+        image: img10,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#742a2a"
+    },
+    {   
+        id:3,
+        name: "Trending Global",
+        image: img16,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#44337a"
+    },
+    {   
+        id:4,
+        name: "Mega Hits,",
+        image: img11,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "Happy Favorites",
+        image: img15,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Shape of You",
+        image: Shu1,
+        file:Shu,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"4:23",
+        author: "Ed Sheeran"
+    },
+    {
+        id:1,
+        name: "Starboy",
+        image: Starimg,
+        file:Star,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:50",
+        author: "The Weeknd"
+    },
+    {
+        id:2,
+        name: "Toxic",
+        image: Txc1img,
+        file:Txc,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:51",
+        author: "BoyWithUke"
+    },
+    {
+        id:3,
+        name: "Heat Waves",
+        image: Heat1,
+        file:Heat,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:55",
+        author: "Glass Animals"
+    },
+    {
+        id:4,
+        name: "I Wanna Be Yours",
+        image: iwbu1,
+        file:Iwbu,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:03",
+        author: "Arctic Monkeys"
+    },
+    {
+        id:5,
+        name: "Birds of a Feather",
+        image: Bff1,
+        file:Bff,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:50",
+        author: "Billie Eilish"
+    },
+    {
+        id:6,
+        name: "Sailor",
+        image: Sail1,
+        file:Sail,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"3:30",
+        author: "Gigi Perez"
+    },
+    {
+        id:7,
+        name: "Sunflower",
+        image: Sun1,
+        file:Sun,
+        desc:"Put a smile on your face with these happy tunes",
+        duration:"2:41",
+        author: "Post Malone"
+    }
+]
